7b35a71c350380805b6c97e129fc8af6f8ac0190
Melvin Adu <contactmelvinadu@gmail.com>
2022-02-16 14:12:01 -0500

    refactor app to only include price data table on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-  
+import React, { useEffect, useState } from 'react';
+import '../App.css';
+
+import axios from 'axios'
+import Token
+ from '../components/Token/Token';
+
 const Home = () => {
+
+  const [tokens,setTokens] = useState([])
+  const [search,setSearch] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res=>{
+       setTokens(res.data)
+       console.log(res.data)
+    }).catch(error=>console.log(error))
+  }, [])
+
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+
+  const filteredTokens = tokens.filter(
+    token=>token.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>Welcome to GeeksforGeeks</h1>
+    <>
+
+    <div className="token-app">
+      <div className="token-search">
+        {/* <h1 className="token-text">Search your desired token</h1> */}
+        <form action="">
+          <input type="text" className="token-input" placeholder="Provide the token name" onChange={handleChange}/>
+
+        </form>
+      </div>
     </div>
+
+      {filteredTokens.map(token=>{
+        return(
+          <Token 
+          key={token.id} 
+          name={token.name} 
+          image={token.image} 
+          symbol={token.symbol}
+          marketcap={token.market_cap}
+          price={token.current_price}
+          pricechange={token.price_change_percentage_24h}
+          volume={token.total_volume}
+          />
+        );
+      })}
+      
+    </>
   );
 };
   
