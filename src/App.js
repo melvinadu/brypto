789ca99832d72ade04f44b6202e088dec4287a93
Melvin Adu <contactmelvinadu@gmail.com>
2022-02-16 14:25:41 -0500

    update app to latest branch

--- src/App.js
+++ src/App.js
@@ -11,27 +11,7 @@ import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
 
 function App() {
-<<<<<<< HEAD
   
-=======
-  const [tokens,setTokens] = useState([])
-  const [search,setSearch] = useState('')
-  useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res=>{
-       setTokens(res.data)
-       console.log(res.data)
-    }).catch(error=>console.log(error))
-  }, [])
-  const handleChange = e =>{
-    setSearch(e.target.value)
-  }
-  const filteredTokens = tokens.filter(token=>
-    token.name.toLowerCase().includes(search.toLowerCase())
-    )
-
-    
->>>>>>> ui/change-elements
   return (
     <>
     <Router>
@@ -47,36 +27,6 @@ function App() {
       </Routes>
     </Router>
     
-<<<<<<< HEAD
-=======
-    <div className="token-app">
-      <div className="token-search">
-        {/* <h1 className="token-text">Search your desired token</h1> */}
-        <form action="">
-          <input type="text" className="token-input" placeholder="Search for a token" onChange={handleChange}/>
-
-        </form>
-
-      </div>
-      {filteredTokens.map(token=>{
-        return(
-          <Token 
-          key={token.id} 
-          name={token.name} 
-          image={token.image} 
-          symbol={token.symbol}
-          marketcap={token.market_cap}
-          price={token.current_price}
-          pricechange={token.price_change_percentage_24h}
-          volume={token.total_volume}
-          />
-        );
-      })}
-
-
-
-    </div>
->>>>>>> ui/change-elements
     </>
   );
 }
