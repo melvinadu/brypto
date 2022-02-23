f216c196cff402eb28c90677ae7f218ec4b77c33
Rishard C <rishard.c@hotmail.com>
2022-02-23 11:48:21 -0500

    adjust styling

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -21,20 +21,21 @@ const Blogs = () => {
         height: '100vh'
       }}
     >
-      <h1>Welcome to GeeksforGeeks Blogs</h1>
+      <div className="feed1">
       <iframe 
        style={{
         display: 'flex',
         flexDirection: "row",
       }}
       width="100%" 
-      img="100px"
+      // img="50px"
       height="750" 
-      frameborder="0" 
-      class="rssdog" 
+      // frameborder="0" 
+      className="feed" 
       src="https://www.rssdog.com/index.php?url=https%3A%2F%2FBlockchain.News%2FRSS%2F&amp;mode=html&amp;showonly=&amp;maxitems=0&amp;showdescs=1&amp;desctrim=0&amp;descmax=0&amp;tabwidth=100%25&amp;utf8=1&amp;linktarget=_blank&amp;bordercol=%23ff0000&amp;headbgcol=ff0000&amp;headtxtcol=%23ffffff&amp;titlebgcol=%23f1eded&amp;titletxtcol=%23000000&amp;itembgcol=%23ffffff&amp;itemtxtcol=%23000000&amp;ctl=0"
       >
       </iframe>
+      </div>
     </div>
   );
 };
