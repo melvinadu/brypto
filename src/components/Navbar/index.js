e994ef42f7cff35032c0a41e15f01ac1acb6a591
Kofi21 <owu.evander@gmail.com>
2022-02-24 13:58:20 -0500

    rearrange order of navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Nav,
   NavLink,
@@ -6,14 +6,14 @@ import {
   NavMenu,
   NavBtn,
   NavBtnLink,
-} from './NavbarElements';
-  
+} from "./NavbarElements";
+
 const Navbar = () => {
   return (
     <>
       <Nav>
         <Bars />
-  
+
         <NavMenu>
           {/* <NavLink to="/" activeStyle>
           <img className="logo" src="logo.png" alt="BigCo Inc. logo"/>
@@ -33,21 +33,23 @@ const Navbar = () => {
           <NavLink to='/team' activeStyle>
             Teams
           </NavLink> */}
-          <NavLink to='/blogs' activeStyle>
-            News
-          </NavLink>
-          <NavLink to='/sign-up' activeStyle>
+
+          <NavLink to="/sign-up" activeStyle>
             Search
           </NavLink>
+          <NavLink to="/blogs" activeStyle>
+            News
+          </NavLink>
+
           {/* Second Nav */}
           {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
         </NavMenu>
         <NavBtn>
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
         </NavBtn>
       </Nav>
     </>
   );
 };
-  
+
 export default Navbar;
